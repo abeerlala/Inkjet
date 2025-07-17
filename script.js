e2885b4e189f93525c968fb2b03a48a0b21dae80
Abeer Khan <140332219+abeerlala@users.noreply.github.com>
2025-07-17 19:07:13 +0500

    changes new

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.carouselRight',
-            prevEl: '.carouselLeft',
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-        speed: 1500
-    });
-});
+// Nav Logic
 
-fetch("/Components/nav.html")
-    .then(res => res.text())
-    .then(data => { document.getElementById("nav").innerHTML = data; })
-    .then(() => {
-        const checkbox = document.querySelector("#menu");
-        const navList = document.querySelector(".navList");
-        const label = document.querySelector(".label");
-        checkbox.addEventListener("change", () => {
-            if (checkbox.checked) {
-                navList.style.display = "flex";
-                label.classList.remove("fa-bars")
-                label.classList.add("fa-xmark")
-            } else {
-                navList.style.display = "none";
-                label.classList.remove("fa-xmark")
-                label.classList.add("fa-bars")
-            }
-        })
-    })
-    .then(() => {
-        const pageName = location.href.split("/").pop();
-        const navLinks = document.querySelectorAll(".navLink");
-        navLinks.forEach((link) => {
-            if (link.getAttribute("href") == pageName) {
-                link.classList.add("activeNavLink")
-            } else if (pageName == "services.html" || pageName == "techSupport.html" || pageName == "training.html") {
-                document.querySelector(".dropdown a").classList.add("activeA");
-            }
-        })
-    })
+const checkbox = document.querySelector("#menu");
+const navList = document.querySelector(".navList");
+const label = document.querySelector(".label");
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        navList.style.display = "flex";
+        label.classList.remove("fa-bars")
+        label.classList.add("fa-xmark")
+    } else {
+        navList.style.display = "none";
+        label.classList.remove("fa-xmark")
+        label.classList.add("fa-bars")
+    }
+})
+
+const pageName = location.href.split("/").pop();
+const navLinks = document.querySelectorAll(".navLink");
+const homeLink = document.querySelector(".navLink");
+
+navLinks.forEach((link) => {
+    if (link.getAttribute("href") == pageName) {
+        link.classList.add("activeNavLink")
+    } else if (pageName == "services.html" || pageName == "techSupport.html" || pageName == "training.html") {
+        document.querySelector(".dropdown a").classList.add("activeA");
+    } else if (pageName == "") {
+        homeLink.classList.add("activeNavLink")
+    }
+})
+
+// ToTop Show/Hide
 
 const toTop = document.querySelector("#toTop");
 document.addEventListener("scroll", () => {
@@ -58,47 +41,56 @@ toTop.addEventListener("click", (e) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+// Home Page Slides
 
+if (pageName == "" || pageName == "index.html") {
+    document.addEventListener('DOMContentLoaded', function () {
+        var swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.carouselRight',
+                prevEl: '.carouselLeft',
+            },
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            speed: 1500
+        });
+    });
 
-
-const bgLayers = document.querySelectorAll('.mainSection .bgLayer');
-const leftBtn = document.getElementById("mainImgLeft");
-const rightBtn = document.getElementById("mainImgRight");
-let currentBg = 0;
-let intervalId;
-
-bgLayers[currentBg].classList.add('active');
-
-function updateActiveSlide(newIndex) {
-    bgLayers.forEach(layer => layer.classList.remove('active'));
-    currentBg = newIndex;
-    bgLayers[currentBg].classList.add('active');
 }
 
-function startAutoSlide() {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-        updateActiveSlide((currentBg + 1) % bgLayers.length);
-    }, 10000);
-}
+// Tabs Functionality 
 
-startAutoSlide();
+const tabs = document.querySelectorAll(".tab");
+const products = document.querySelectorAll(".product");
+let category = "All";
 
-rightBtn.addEventListener("click", () => {
-    if (currentBg < bgLayers.length - 1) {
-        updateActiveSlide(currentBg + 1);
-        startAutoSlide();
-    } else if (currentBg = 4) {
-        updateActiveSlide(0);
-        startAutoSlide();
-    }
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("activeTab"));
+        tab.classList.add("activeTab");
+        category = tab.id;
+        products.forEach(product => {
+            const productCategory = product.dataset.category;
+
+            if (category === "All" || productCategory === category) {
+                product.style.display = "flex";
+            } else {
+                product.style.display = "none";
+            }
+        });
+    });
 });
 
-leftBtn.addEventListener("click", () => {
-    if (currentBg > 0) {
-        updateActiveSlide(currentBg - 1);
-        startAutoSlide();
-    } else if (currentBg == 0) {
-        updateActiveSlide(4);
-    }
+const allProducts = document.querySelectorAll(".product");
+
+allProducts.forEach(product => {
+    const name = product.querySelector("h2").innerText;
+    product.querySelector("a").href = product.querySelector("a").href + " "+ name ;
 });
