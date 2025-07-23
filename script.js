514aee91a89ba14234fc387a00e828ea5aca3d33
Abeer Khan <140332219+abeerlala@users.noreply.github.com>
2025-07-23 18:56:50 +0500

    files

---
+++ script.js
@@ -0,0 +1,63 @@
+// ToTop Show/Hide
+
+const toTop = document.querySelector("#toTop");
+document.addEventListener("scroll", () => {
+    scrollY > 300 ? toTop.style.display = "grid" : toTop.style.display = "none";
+});
+
+toTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Home Page Slides
+
+const pageName = location.href.split("/").pop();
+
+if (pageName == "" || pageName == "index.html") {
+    document.addEventListener('DOMContentLoaded', function () {
+        var swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.carouselRight',
+                prevEl: '.carouselLeft',
+            },
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            speed: 1500
+        });
+    });
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").style.display = "flex";;
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown").style.display = "none";;
+}
+
+// Product Cards Logic
+
+const images = document.querySelectorAll(".imageProduct img");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        const src = img.src;
+        window.open(src)
+    });
+});
+
+const productCard = document.querySelectorAll(".productCard");
+
+productCard.forEach(card => {
+    const img = card.querySelector("img");
+    const a = card.querySelector("a");
+    a.href = `${a.href}Hello, I’m interested in the ${img.alt}. Can you please share the price? Kindly send me the price and availability of the ${img.alt}`;
+});
